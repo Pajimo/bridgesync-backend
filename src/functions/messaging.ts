1b87f6ed831503963dddcdc69f845696857a7f08
Pajimo <faniyanolamide@gmail.com>
2024-03-10 13:27:25 +0000

    feat: complete

--- src/functions/messaging.ts
+++ src/functions/messaging.ts
@@ -6,6 +6,8 @@ import {
 } from "@azure/functions";
 import OpenAI from "openai";
 import { connectToUserDatabase } from "../../utils/database";
+import { containsKeyword } from "../../utils/constants";
+import axios from "axios";
 export async function messaging(
   request: HttpRequest,
   context: InvocationContext
@@ -14,7 +16,14 @@ export async function messaging(
 
   const userMessage: any = await request.json();
 
-  const { message, userType, channelId } = userMessage;
+  const { message, userName, channelId } = userMessage;
+
+  const indexUrl = `https://chatbot-knowledgebase.search.windows.net/indexes/bridgesync-source/docs?search=${message}&api-version=2023-11-01`;
+
+  const headers = {
+    "Content-Type": "application/json",
+    "api-key": process.env.AZURE_AI_SEARCH_KEY,
+  };
 
   const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
@@ -27,39 +36,56 @@ export async function messaging(
       spaceId: "8d692ffde822-43f8-934a-9152c09b2d44",
     });
 
+    const messageForSyncAI = containsKeyword(message.toLocaleLowerCase());
+
     let channelData = workspaceData.channels.filter(
       (id) => id.id === channelId
     );
 
     let [newChannelList] = channelData;
 
-    channelData[0]?.messages.length < 1 &&
-      channelData[0].messages.push({
-        role: "system",
-        content:
-          "You are a very hardworking medical assistant called SyncAI who helps medical practitioners with questions regarding surgery, best practices, drugs, trials and many more medical helps.",
-        name: "SyncAI",
-      });
+    if (channelData[0]?.messages.length < 1) {
+      await axios
+        .get(indexUrl, { headers })
+        .then((response) => {
+          //   console.log("Search Results:", response.data.value);
+          const { value } = response.data;
+
+          channelData[0].messages.push({
+            role: "system",
+            content:
+              `You are a very hardworking medical assistant called SyncAI who helps medical practitioners with questions regarding surgery, best practices, drugs, trials and many more medical helps. Only use the following information to answer questions. try to mention the users name in your reply as often as you can for example, hello "the users namer" how are you today. If the answer is not included, say exactly Hmm, I am not sure. and stop after that. Refuse to answer any question not about the info. Never break character. the following should be the knowledge base to use` +
+              value.map((item) => item.content).join(" ") +
+              "Please ensure that every piece of information or claim in the summary is accompanied by a reference to the specific article it was derived from. Use inline citations for these references, and format them as simple parenthetical references (e.g., [Article 1], [Article 2]) immediately following the related information.",
+            name: "SyncAI",
+          });
+        })
+        .catch((error) => {
+          console.error("Error performing search query:", error.response);
+        });
+    }
 
     const newMsg = {
       role: "user",
       content: message,
-      name: "Olamide",
+      name: userName,
     };
 
     channelData[0].messages?.push(newMsg);
 
-    const response = await openai.chat.completions.create({
-      model: "gpt-4-0125-preview",
-      messages: channelData[0].messages,
-      temperature: 1,
-    });
+    if (messageForSyncAI) {
+      const response = await openai.chat.completions.create({
+        model: "gpt-4-0125-preview",
+        messages: channelData[0].messages,
+        temperature: 1,
+      });
 
-    channelData[0].messages?.push({
-      role: "assistant",
-      content: response.choices[0].message.content,
-      name: "SyncAI",
-    });
+      channelData[0].messages?.push({
+        role: "assistant",
+        content: response.choices[0].message.content,
+        name: "SyncAI",
+      });
+    }
 
     await workspaceCollection.updateOne(
       { _id: workspaceData._id },
